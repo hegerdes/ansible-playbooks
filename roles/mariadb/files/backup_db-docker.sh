f65a9faed64df3d483a14008e4231fb7d9ddd384
Henrik Gerdes <hegerdes@outlook.de>
2022-01-16 14:57:51 +0100

    Various fixes

--- roles/mariadb/files/backup_db-docker.sh
+++ roles/mariadb/files/backup_db-docker.sh
@@ -4,3 +4,6 @@ LOGFILE=/db_dumps/db_backup.log
 echo -n "Creating db dump to: /db_dumps/$(date '+%Y-%m-%d').sql..." >> $LOGFILE
 docker exec mariadb bash -c '/usr/bin/mysqldump --all-databases --password=$MYSQL_ROOT_PASSWORD' > /db_dumps/$(date '+%Y-%m-%d')-${HOSTNAME}.sql
 echo "done" >> $LOGFILE
+
+# Restore:
+# docker exec mariadb bash -c 'exec mysql -uroot -p$MYSQL_ROOT_PASSWORD' < <NAME_OF_FILE>
--- roles/mariadb/files/backup_db.sh
+++ roles/mariadb/files/backup_db.sh
@@ -4,3 +4,6 @@ LOGFILE=/db_dumps/db_backup.log
 echo -n "Creating db dump to: /db_dumps/$(date '+%Y-%m-%d').sql..." >> $LOGFILE
 mysqldump --all-databases > /db_dumps/$(date '+%Y-%m-%d')-${HOSTNAME}.sql
 echo "done" >> $LOGFILE
+
+# Restore
+mysql < <NAME_OF_FILE>
