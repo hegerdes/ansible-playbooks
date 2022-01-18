cd5014d58db0afac29e7585b83051db897689211
Henrik Gerdes <hegerdes@outlook.de>
2022-01-18 20:38:18 +0100

    Bugfixes and inventory uses v-net ips

--- roles/mariadb/files/backup_db.sh
+++ roles/mariadb/files/backup_db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 LOGFILE=/db_dumps/db_backup.log
-echo -n "Creating db dump to: /db_dumps/$(date '+%Y-%m-%d').sql..." >> $LOGFILE
+echo -n "Creating db dump to: /db_dumps/$(date '+%Y-%m-%d')-${HOSTNAME}.sql..." >> $LOGFILE
 mysqldump --all-databases > /db_dumps/$(date '+%Y-%m-%d')-${HOSTNAME}.sql
 echo "done" >> $LOGFILE
 
 # Restore
-mysql < <NAME_OF_FILE>
+# mysql < <NAME_OF_FILE>
