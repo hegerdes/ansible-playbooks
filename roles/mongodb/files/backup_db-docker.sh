3abe64290323ff381f91ea0bc719ca857498aa52
Henrik Gerdes <hegerdes@outlook.de>
2022-05-20 12:40:25 +0200

    Use pigz for compression if avaiable

--- roles/mongodb/files/backup_db-docker.sh
+++ roles/mongodb/files/backup_db-docker.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+ZIPPER=pigz
 BACKUP_DIR=${CUSTOM_BACKUP_DIR:="/backup_dumps"}
 DATE_STR=$(date '+%Y-%m-%d')
 LOGFILE="${BACKUP_DIR}/db_backup.log"
 
+if ! [ -x "$(command -v pigz)" ]; then
+  echo 'Error: pigz is not installed! Fallback to gzip' | tee -a $LOGFILE
+  ZIPPER=gzip
+fi
+
 echo -n "Creating db dump to: ${BACKUP_DIR}/${DATE_STR}-${HOSTNAME}.dump.tar..." | tee -a $LOGFILE
-docker exec mongodb sh -c 'exec mongodump -u=$MONGO_INITDB_ROOT_USERNAME -p=$MONGO_INITDB_ROOT_PASSWORD --archive' | gzip > ${BACKUP_DIR}/${DATE_STR}-${HOSTNAME}.dump.tar.gz
+docker exec mongodb sh -c 'exec mongodump -u=$MONGO_INITDB_ROOT_USERNAME -p=$MONGO_INITDB_ROOT_PASSWORD --archive' | $ZIPPER > ${BACKUP_DIR}/${DATE_STR}-${HOSTNAME}.dump.tar.gz
 echo "done" | tee -a $LOGFILE
 
 # Restore
--- roles/mongodb/files/backup_db.sh
+++ roles/mongodb/files/backup_db.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+ZIPPER=pigz
 BACKUP_DIR=${CUSTOM_BACKUP_DIR:="/backup_dumps"}
 DATE_STR=$(date '+%Y-%m-%d')
 LOGFILE="${BACKUP_DIR}/db_backup.log"
 
+if ! [ -x "$(command -v pigz)" ]; then
+  echo 'Error: pigz is not installed! Fallback to gzip' | tee -a $LOGFILE
+  ZIPPER=gzip
+fi
+
 echo -n "Creating db dump to: ${BACKUP_DIR}/${DATE_STR}-${HOSTNAME}.dump.tar..." | tee -a $LOGFILE
-mongodump -u=$MONGO_INITDB_ROOT_USERNAME -p=$MONGO_INITDB_ROOT_PASSWORD --archive | gzip > ${BACKUP_DIR}/${DATE_STR}-${HOSTNAME}.dump.tar
+mongodump -u=$MONGO_INITDB_ROOT_USERNAME -p=$MONGO_INITDB_ROOT_PASSWORD --archive | $ZIPPER > ${BACKUP_DIR}/${DATE_STR}-${HOSTNAME}.dump.tar
 echo "done" | tee -a $LOGFILE
